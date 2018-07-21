e7405154d23aeb148c9f119806e5f3ddc08e4c9b
Mark Raynsford <code@io7m.com>
2018-07-21 15:50:15 +0100

    Initial example

---
+++ src/main/java/com/io7m/javafx0/Main.java
@@ -0,0 +1,24 @@
+package com.io7m.javafx0;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public final class Main extends Application
+{
+  public Main()
+  {
+
+  }
+
+  @Override
+  public void start(final Stage stage)
+    throws Exception
+  {
+
+  }
+
+  public static void main(final String[] args)
+  {
+
+  }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module com.io7m.javafx0
+{
+  opens com.io7m.javafx0 to javafx.graphics;
+
+  requires javafx.graphics;
+}
